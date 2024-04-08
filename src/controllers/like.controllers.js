2512fa15efdfe7cb0d9077e8a835f475a0ff2df1
Abuzar Zafar <sume62800@gmail.com>
2024-04-08 09:36:57 +0530

    debug thelikes controllers

--- src/controllers/like.controllers.js
+++ src/controllers/like.controllers.js
@@ -8,65 +8,60 @@ const toggleVideoLike = asyncHandler(async (req, res) => {
     const {videoId} = req.params
     //TODO: toggle like on video
 
-    const toggleLike=Like.create({
+    const toggleLike=await Like.create({
         video: videoId,
         likedBy: req.user?._id
     })
 
-    const saveVideoLike=await toggleLike.save().select("_id")
-
-    return res.status(200).json(new apiResponse(200,{likeId:saveVideoLike._id},"video like saved!")) 
+    return res.status(200).json(new apiResponse(200,{likeId:toggleLike},"video like saved!")) 
 })
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
     const {commentId} = req.params
     //TODO: toggle like on comment
 
-    const toggleCommentLike=Like.create({
+    const toggleCommentLike=await Like.create({
         comment: commentId,
         likedBy: req.user?._id
     })
 
-    const saveCommentLike=await toggleCommentLike.save().select("_id")
-
-    return res.status(200).json(new apiResponse(200,{commentId:saveCommentLike._id},"comment like saved!")) 
+    return res.status(200).json(new apiResponse(200,{toggleCommentLike},"comment like saved!")) 
 })
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
     const {tweetId} = req.params
     //TODO: toggle like on tweet
 
-    const toggleTweetLike=Like.create({
+    const toggleTweetLike=await Like.create({
         tweet: tweetId,
         likedBy: req.user?._id
     })
 
-    const saveTweetLike=await toggleTweetLike.save().select("_id")
 
 
-    return res.status(200).json(new apiResponse(200,{tweetId:saveTweetLike._id},"tweet like saved!")) 
+    return res.status(200).json(new apiResponse(200,{toggleTweetLike},"tweet like saved!")) 
 }
 )
 
 const getLikedVideos = asyncHandler(async (req, res) => {
     //TODO: get all liked videos
 
-    const likedVideos = await Like.aggreate([
+    const likedVideos = await Like.aggregate([
         {
             $match:{
-                likedBy:req.user?._id
+                likedBy: new mongoose.Types.ObjectId(req.user?._id)
             }
         },
-        {
-            $match: {
-              video: { $type: "string", $ne: "" } // Checking if fieldToCheck is of type string and is not an empty string
-            }
-        },
-        {
-            $project:{
-                video:1
-            }
-        }
+        // {
+        //     $match: {
+        //       video: { $type: "string", $ne: "" } // Checking if fieldToCheck is of type string and is not an empty string
+        //     }
+        // },
+        // {
+        //     $project:{
+        //         video:1
+        //     }
+        // }
     ])
 
     return res.status(200).json(new apiResponse(200,{likedVideos},"liked videos fetch")) 
